ca1ba901d7bca97ad60f8eadaf6c2d15ba60b0f6
Joey S <gcam022005@coderacademy.edu.au>
2020-12-16 21:58:01 +1100

    Put questionairre into a loop and added a restart feature

--- bad_film_finder.rb
+++ bad_film_finder.rb
@@ -22,71 +22,82 @@ ascii_title
 
 prompt = TTY::Prompt.new(active_color: :green)
 
-init_question = prompt.select(Rainbow("Do you want to watch a terrible film?").underline.magenta, %w(Yes No))
+while prompt
+    init_question = prompt.select(Rainbow("Do you want to watch a terrible film?").underline.magenta, %w(Yes No))
 
-if init_question == "No"
-        good_movie_lover
-end
+    if init_question == "No"
+         good_movie_lover
+    end
 
-fav_genre = prompt.select(Rainbow("Great to hear! What's your (least) favourite genre?").underline.magenta, %w(Horror Comedy Drama Action Adventure))
+    fav_genre = prompt.select(Rainbow("Great to hear! What's your (least) favourite genre?").underline.magenta, %w(Horror Comedy Drama Action Adventure))
 
-fav_type = prompt.select(Rainbow("Finally, what do you want your cinematic experience to be?").underline.magenta, ["I want to laugh", "Make me cringe", "I really enjoy embarrassing myself infront of all my friends and peers", "I need a nap"]) #Gotta figure out how to make these array arguments into a sentence
+    fav_type = prompt.select(Rainbow("Finally, what do you want your cinematic experience to be?").underline.magenta, ["I want to laugh", "Make me cringe", "I really enjoy embarrassing myself infront of all my friends and peers", "I need a nap"]) #Gotta figure out how to make these array arguments into a sentence
 
-if fav_type == "I want to laugh"
-    fav_type = "Laugh"
-elsif fav_type == "Make me cringe"
-     fav_type = "Cringe"
-elsif fav_type == "I really enjoy embarrassing myself infront of all my friends and peers"
-    fav_type = "Embarrass"
-elsif fav_type == "I need a nap"
-    fav_type = "Sleep"
-end
+    if fav_type == "I want to laugh"
+        fav_type = "Laugh"
+    elsif fav_type == "Make me cringe"
+        fav_type = "Cringe"
+    elsif fav_type == "I really enjoy embarrassing myself infront of all my friends and peers"
+        fav_type = "Embarrass"
+    elsif fav_type == "I need a nap"
+        fav_type = "Sleep"
+    end
 
-puts Rainbow("So you've selected #{fav_genre} and #{fav_type}...").green.underline
-sleep 1
-print "You should watch "
+    puts Rainbow("So you've selected #{fav_genre} and #{fav_type}...").green.underline
+    sleep 1
+    print "You should watch "
 
-#This is where the app accesses the films dependent on the user input. It is extremely ugly, but it works
-if fav_genre == "Horror" && fav_type == "Laugh"
-    puts movies[:horror][:laugh].sample
-elsif fav_genre == "Horror" && fav_type == "Cringe"
-    puts movies[:horror][:cringe].sample
-elsif fav_genre == "Horror" && fav_type == "Embarrass"
-    puts movies[:horror][:embarrass].sample
-elsif fav_genre == "Horror" && fav_type == "Sleep"
-    puts movies[:horror][:boredom].sample
-elsif fav_genre == "Comedy" && fav_type == "Laugh"
+        #This is where the app accesses the films dependent on the user input. It is extremely ugly, but it works
+    if fav_genre == "Horror" && fav_type == "Laugh"
+        puts movies[:horror][:laugh].sample
+    elsif fav_genre == "Horror" && fav_type == "Cringe"
+        puts movies[:horror][:cringe].sample
+    elsif fav_genre == "Horror" && fav_type == "Embarrass"
+        puts movies[:horror][:embarrass].sample
+    elsif fav_genre == "Horror" && fav_type == "Sleep"
+        puts movies[:horror][:boredom].sample
+    elsif fav_genre == "Comedy" && fav_type == "Laugh"
         puts movies[:comedy][:laugh].sample
-elsif fav_genre == "Comedy" && fav_type == "Cringe"
+    elsif fav_genre == "Comedy" && fav_type == "Cringe"
         puts movies[:comedy][:cringe].sample
-elsif fav_genre == "Comedy" && fav_type == "Embarrass"
+    elsif fav_genre == "Comedy" && fav_type == "Embarrass"
         puts movies[:comedy][:embarrass].sample
-elsif fav_genre == "Comedy" && fav_type == "Sleep"
+    elsif fav_genre == "Comedy" && fav_type == "Sleep"
         puts movies[:comedy][:boredom].sample
-elsif fav_genre == "Drama" && fav_type == "Laugh"
+    elsif fav_genre == "Drama" && fav_type == "Laugh"
         puts movies[:drama][:laugh].sample
-elsif fav_genre == "Drama" && fav_type == "Cringe"
+    elsif fav_genre == "Drama" && fav_type == "Cringe"
         puts movies[:drama][:cringe].sample
-elsif fav_genre == "Drama" && fav_type == "Embarrass"
+    elsif fav_genre == "Drama" && fav_type == "Embarrass"
         puts movies[:drama][:embarrass].sample
-elsif fav_genre == "Drama" && fav_type == "Sleep"
+    elsif fav_genre == "Drama" && fav_type == "Sleep"
         puts movies[:drama][:boredom].sample
-elsif fav_genre == "Action" && fav_type == "Laugh"
+    elsif fav_genre == "Action" && fav_type == "Laugh"
         puts movies[:action][:laugh].sample
-elsif fav_genre == "Action" && fav_type == "Cringe"
+    elsif fav_genre == "Action" && fav_type == "Cringe"
         puts movies[:action][:cringe].sample
-elsif fav_genre == "Action" && fav_type == "Embarrass"
+    elsif fav_genre == "Action" && fav_type == "Embarrass"
         puts movies[:action][:embarrass].sample
-elsif fav_genre == "Action" && fav_type == "Sleep"
+    elsif fav_genre == "Action" && fav_type == "Sleep"
         puts movies[:action][:boredom].sample
-elsif fav_genre == "Adventure" && fav_type == "Laugh"
+    elsif fav_genre == "Adventure" && fav_type == "Laugh"
         puts movies[:adventure][:laugh].sample
-elsif fav_genre == "Adventure" && fav_type == "Cringe"
+    elsif fav_genre == "Adventure" && fav_type == "Cringe"
         puts movies[:adventure][:cringe].sample
-elsif fav_genre == "Adventure" && fav_type == "Embarrass"
+    elsif fav_genre == "Adventure" && fav_type == "Embarrass"
         puts movies[:adventure][:embarrass].sample
-elsif fav_genre == "Adventure" && fav_type == "Sleep"
+    elsif fav_genre == "Adventure" && fav_type == "Sleep"
         puts movies[:adventure][:boredom].sample
-else
-    puts "you should have found this on purpose"
+    else
+        puts "you should have found this on purpose"
+    end
+
+    retry_question = prompt.select(Rainbow("Did you want to start again?").underline.magenta, %w(Yes No))
+    if retry_question == "No"
+        puts Rainbow("Please enjoy the movie! (or don't)").magenta
+        exit
+    end
+    puts Rainbow("Not good (bad) enough for you eh? Lets find you another one!").red
+    sleep 2
+    puts ""
 end
